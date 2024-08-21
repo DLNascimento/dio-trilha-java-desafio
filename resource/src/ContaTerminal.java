5e9e912511fbc00297ad2c0bebfe902d15b28998
Diego Nascimento <nascimentodln22@gmail.com>
2024-08-21 13:43:27 -0300

    Project Finish

---
+++ resource/src/ContaTerminal.java
@@ -0,0 +1,9 @@
+public class ContaTerminal {
+
+    int numero;
+    String agencia;
+    String nomeCliente;
+    double saldo;
+
+
+}
--- resource/src/Main.java
+++ resource/src/Main.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+
+        Scanner scanner = new Scanner(System.in);// CRIANDO O SCANNER
+        ContaTerminal conta = new ContaTerminal(); // CRIANDO O OBJETO DA CLASSE CONTA TERMINAL
+
+
+        // PASSANDO OS DADOS DO USUARIO
+        System.out.println("Por favor, insira o numero de sua conta: ");
+        conta.numero = scanner.nextInt();
+        System.out.println("Por favor, insira o numero de sua agencia: ");
+        conta.agencia = scanner.next();
+        System.out.println("Por favor, insira seu nome: ");
+        conta.nomeCliente = scanner.next();
+        System.out.println("Por favor, insira o valor de saldo: ");
+        conta.saldo = scanner.nextDouble(); // ATENÇÃO AO COLOCAR O VALOR DO SALDO, POIS PRECISA SER COM VIRGULA(,)
+
+        // IMPRIMINDO OS DADOS DO CLIENTE
+        System.out.println("Cliente: " + " " + conta.nomeCliente);
+        System.out.println("Agência: " + " " + conta.agencia);
+        System.out.println("Conta: " + " " + conta.numero);
+        System.out.println("Saldo: " + " " + conta.saldo);
+
     }
 }
